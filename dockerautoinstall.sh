a9a36e4977f9bb17d30d262f141c01ad5322a79c
Sleek <42476966+s4dic@users.noreply.github.com>
2022-11-05 18:28:05 +0100

    Update dockerautoinstall.sh
    correct some bug with debian

--- dockerautoinstall.sh
+++ dockerautoinstall.sh
@@ -12,9 +12,13 @@ case $version in
     apt install -y ca-certificates curl gnupg lsb-release
     mkdir -p /etc/apt/keyrings && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-	apt update && chmod a+r /etc/apt/keyrings/docker.gpg && apt update && apt install -y docker-ce docker-ce-cli docker-compose containerd.io docker-compose-plugin
-    echo "vérification de la version installée:"
+	apt update && chmod a+r /etc/apt/keyrings/docker.gpg && apt update && apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  curl -SL https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+  sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+  chmod +x /usr/bin/docker-compose
+    echo "vérification de la version docker + docker compose installée:"
     docker --version
+    docker-compose --version
     echo "fin de l'installation"
     ;;
   debian)
@@ -22,13 +26,17 @@ case $version in
 		echo "Please login with root account for Debian"
 		exit 0
 	fi
-    apt remove -y docker docker-ce docker-ce-cli docker-ce-rootless-extras docker-compose docker-scan-plugin docker-engine docker.io containerd runc
-    apt update && apt install ca-certificates curl gnupg lsb-release && mkdir -p /etc/apt/keyrings
+    apt remove -y docker docker-ce docker-ce-cli docker-ce-rootless-extras docker-compose docker.io containerd runc
+    apt update && apt install -y ca-certificates curl gnupg lsb-release && mkdir -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-  	apt update && chmod a+r /etc/apt/keyrings/docker.gpg && apt update && apt install -y docker-ce docker-compose docker-ce-cli containerd.io docker-compose-plugin
-    echo "vérification de la version installée:"
+  	apt update && chmod a+r /etc/apt/keyrings/docker.gpg && apt update && apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    curl -SL https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+    ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+    chmod +x /usr/bin/docker-compose
+    echo "vérification de la version docker + docker compose installée:"
     docker --version
+    docker-compose --version
     echo "fin de l'installation"
     ;;
   *)
