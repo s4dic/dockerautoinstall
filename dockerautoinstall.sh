590e4a4234811d1771140ed2b95b9637e23b7b7e
Sleek <42476966+s4dic@users.noreply.github.com>
2023-11-26 16:35:30 +0100

    Update dockerautoinstall.sh
    add docker-scan-plugin support

--- dockerautoinstall.sh
+++ dockerautoinstall.sh
@@ -12,7 +12,7 @@ case $version in
     apt install -y ca-certificates curl gnupg lsb-release
     mkdir -p /etc/apt/keyrings && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-	apt update && chmod a+r /etc/apt/keyrings/docker.gpg && apt update && apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+	apt update && chmod a+r /etc/apt/keyrings/docker.gpg && apt update && apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-scan-plugin
   curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
   sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
   chmod +x /usr/bin/docker-compose
@@ -30,7 +30,7 @@ case $version in
     apt update && apt install -y ca-certificates curl gnupg lsb-release && mkdir -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-  	apt update && chmod a+r /etc/apt/keyrings/docker.gpg && apt update && apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  	apt update && chmod a+r /etc/apt/keyrings/docker.gpg && apt update && apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-scan-plugin
     curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
     ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
     chmod +x /usr/bin/docker-compose
